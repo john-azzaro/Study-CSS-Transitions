470ebbae63af5703f15a1ef5f64967a279d18cce
John Azzaro <jazzaro@temple.edu>
2019-07-29 12:08:57 -0700

    testing scroll functionality

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
 
-function bgChanger() {
-    console.log('working')
+function bgChanger() {                                    // When bgChanger runs...
+    if(this.scrollY) {                                       //  if the window is scrolling vertically... note here that you can use either "window" or "this" but since this function is connected to handleScroll, we already know we are working with window.
+
+    }
 }
 
-function handleScroll() {
-    $(window).on('scroll', bgChanger)
+function handleScroll() {                                 // In the case of the the handleScroll event listener...
+    $(window).on('scroll', bgChanger)                      // if there is a scroll in the window, run bgChanger.
 }
 
-function setupEventListeners() {
+function setupEventListeners() {                           // setup your event listeners, in this case what we are listening for is the user scrolling
     handleScroll();
 }
 
-function initialize() {
+function initialize() {                                     // the only initialization we have is setting up an event listener for the scroll.
     setupEventListeners();
 }
 
-$(initialize)
+$(initialize)                                              // intialize the app
