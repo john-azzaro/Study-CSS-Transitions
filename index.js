eb92e50ca4deaca241eacb8f852b77a789dc5274
John Azzaro <jazzaro@temple.edu>
2019-07-29 13:48:17 -0700

    fixed scroll issue and scroll issue, html, etc.

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
+
+
+
+
+
+
+// function bgChanger() {
+//     if(this.scrollY > this.innerHeight / 2) {
+//         console.log('this triggers me');
+//        $('body').addClass("bg-active");
+//     }
+// }
+
+// window.addEventListener("scroll", bgChanger);
+
+
 'use strict';
 
-function bgChanger() {                                    // When bgChanger runs...
-    if(this.scrollY) {                                       //  if the window is scrolling vertically... note here that you can use either "window" or "this" but since this function is connected to handleScroll, we already know we are working with window.
-
-    }
+function bgChanger() {                
+    if(window.scrollY > window.innerHeight / 2) {
+        $('body').addClass('bg-active');
+    } else {
+        $('body').removeClass('bg-active');
+    }         
 }
 
-function handleScroll() {                                 // In the case of the the handleScroll event listener...
-    $(window).on('scroll', bgChanger)                      // if there is a scroll in the window, run bgChanger.
+function handleScroll() {
+    $(window).on('scroll', bgChanger)
 }
 
-function setupEventListeners() {                           // setup your event listeners, in this case what we are listening for is the user scrolling
+function setupEventListeners() {
     handleScroll();
 }
 
-function initialize() {                                     // the only initialization we have is setting up an event listener for the scroll.
+function initialize() {
     setupEventListeners();
 }
 
-$(initialize)                                              // intialize the app
+$(initialize)
+
+
