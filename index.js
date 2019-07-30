7689bb20890dde91223f3824aca70ca6d7053365
John Azzaro <jazzaro@temple.edu>
2019-07-30 16:16:21 -0700

    improved css, js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 'use strict';
 
-function bgChanger() {                
-    if(window.scrollY > window.innerHeight / 4) {
-        $('body').addClass('bg-active');
-    } else {
-        $('body').removeClass('bg-active');
+function backgroundChange() {                             // when backgroundChange runs...
+    if(window.scrollY > window.innerHeight / 4) {           // if the user scroll is greater than the inner height of the window (divided by 4 so it changes at a lter point in the scoll)
+        $('body').addClass('background-active');             // add class "background-active" which will add a white color instead of the gradient.
+    } else {                                              // if the user scrolls back up...
+        $('body').removeClass('background-active');         // remove the white background color.
     }         
 }
 
-function handleScroll() {
-    $(window).on('scroll', bgChanger)
+function handleScroll() {                                 // for the scroll listener...
+    $(window).on('scroll', backgroundChange)                         // if anywhere in the window the user scrolls, run backgroundChange
 }
 
-function setupEventListeners() {
+function setupEventListeners() {                           // In this case, we have only one event listener (i.e. when the user scrolls up or down)
     handleScroll();
 }
 
-function initialize() {
+function initialize() {                                    // on initialization, setup event listeners...
     setupEventListeners();
 }
 
-$(initialize)
+$(initialize)                                              // Initialize the app...
 
 
